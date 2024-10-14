ab7bd9bdef2a4a55c0a4d9ebf9a4632fea6ada55
Arsalan Anwar <arsalan.anwar.7777@gmail.com>
2024-10-14 00:25:54 -0400

    Black reformatting updations

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class Question(models.Model):
     question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return self.question
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
